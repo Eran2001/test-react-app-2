7d526ca83fad973ad64532b3a4ba3bdeac77d710
Eran <eran.hasarali.2001@gmail.com>
2025-01-29 17:30:36 +0530

    add styles to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,55 @@
+import { Link } from "react-router-dom";
+import "./Footer.css";
+
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-center">Footer</h1>
-    </div>
+    <footer className="bg-white shadow-sm dark:bg-white">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <Link
+            to="https://flowbite.com/"
+            className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+          >
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-8"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-[#000]">
+              Flowbite
+            </span>
+          </Link>
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-[#000] sm:mb-0 dark:text-[#000]">
+            <li>
+              <Link to="/about" className="hover:underline me-4 md:me-6">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className="hover:underline me-4 md:me-6">
+                Privacy Policy
+              </Link>
+            </li>
+            <li>
+              <Link to="#" className="hover:underline me-4 md:me-6">
+                Licensing
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" className="hover:underline">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-[#000] lg:my-8" />
+        <span className="block text-sm text-[#000] sm:text-center dark:text-[#000]">
+          ©{currentYear} . All Rights Reserved.
+        </span>
+      </div>
+    </footer>
   );
 };
 
